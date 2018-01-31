3e40c176058cf78eb21d9eced803ddad89fd7ab2
Joel Trollheden <joel@trollheden.com>
2018-01-31 11:11:11 +0100

    Uppgift1

--- scratch.py
+++ scratch.py
@@ -1,13 +1,22 @@
 import numpy as np
+import numpy.linalg as lalg
 
 
 def read_coordinate_file(filename):
-    fileid=open(filename,'r')
+    global coordinates
+    fileid=open(filename, 'r')
+    num_lines = sum(1 for line in open(filename))
+    coordinates=np.zeros((num_lines,2))
+    i=0
     for line in fileid:
-        tempCord=line.split
-        coordinates=tempCord.float
+        temp=line.split(',')
+        x=float(temp[0][1:])
+        y=float(temp[1][:-2])
+        coordinates[i, :] = [x, y]
+        i+=1
+
 
 read_coordinate_file('SampleCoordinates.txt')
 
 
-
+print(coordinates)
