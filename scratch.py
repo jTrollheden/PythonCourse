90c128149a70fa357b7f482843a75784b629e1cb
Joel Trollheden <joel@trollheden.com>
2018-01-31 11:49:22 +0100

    Upload

--- scratch.py
+++ scratch.py
@@ -1,22 +1,33 @@
 import numpy as np
-import numpy.linalg as lalg
 
-
+#Uppgift 1, läser in, räknar raderna och använder det sedan som ett index för vilken rad koordinaterna motsvarar
 def read_coordinate_file(filename):
-    global coordinates
+    global coord_list
     fileid=open(filename, 'r')
     num_lines = sum(1 for line in open(filename))
-    coordinates=np.zeros((num_lines,2))
+    coord_list=np.zeros((num_lines,2))
     i=0
     for line in fileid:
         temp=line.split(',')
         x=float(temp[0][1:])
         y=float(temp[1][:-2])
-        coordinates[i, :] = [x, y]
+        coord_list[i, :] = [x, y]
         i+=1
+
+#Uppgift 2 - Punkterna plottas ut
+import matplotlib.pyplot as plt
+
+def plot_points(coord_list):
+    fig1 = plt.figure()
+    fig1.gca()  # Only needed for the ipython %matplotlib inline to display something
+    plt.plot(coord_list,".b")
+    plt.show()
+
 
 
 read_coordinate_file('SampleCoordinates.txt')
+print(coord_list)
 
+plot_points(coord_list)
 
-print(coordinates)
+#Uppgift 3 -
