cdb2baa6d6ff9b8bd5f9b6be5d10b8987982c2ee
Joel Trollheden <joel@trollheden.com>
2018-02-02 10:50:04 +0100

    nu

---
+++ inlamnn.py
@@ -0,0 +1,180 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+from scipy.sparse import csr_matrix
+import matplotlib.collections
+from scipy.sparse.csgraph import dijkstra
+import time
+from scipy import spatial
+
+#input to program
+file_to_read = input('What country do you wish to travel in(g,h,s):')
+stad_1 = input('From which city do you want to travel:')
+stad_2 = input('To which city do you want to travel:')
+
+if file_to_read == 'g':
+    file_to_read = 'GermanyCities.txt'
+    radius_search = 0.0025
+elif file_to_read == 'h':
+    file_to_read = 'HungaryCities.txt'
+    radius_search = 0.005
+else:
+    file_to_read = 'SampleCoordinates.txt'
+    radius_search = 0.08
+
+start_time = time.time()
+
+#Reads coordinates from file
+def read_coordinate_file(file_name):
+    file1 = open(file_name, 'r')
+    coordinate_list_up = ([])
+
+    for line in file1:
+        coordinate_list_up.append(line)
+    file1.close()
+    return coordinate_list_up
+
+#Plots the things, the cities, the cities that are connected, and the path between two chosen cities
+def plot_points(coord_list, connected_cities, path):
+
+    all_coords = ([])
+
+    for i_1 in range(len(connected_cities)):
+        temp_coords = [(coord_list[connected_cities[i_1][1]]), (coord_list[connected_cities[i_1][0]])]
+        all_coords.append(temp_coords)
+    lines1 = all_coords
+    lc1 = matplotlib.collections.LineCollection(lines1, linestyles="-", linewidth=0.3)
+    fig, ax = plt.subplots()
+    ax.add_collection(lc1)
+
+    all_coords2 = ([])
+    for i_2 in range(len(path)-1):
+        temp_coords = [(coord_list[path[i_2]]), (coord_list[path[i_2+1]])]
+        all_coords2.append(temp_coords)
+    lc2 = matplotlib.collections.LineCollection(all_coords2, linestyles="-", linewidth=1.2, colors='red')
+    ax.add_collection(lc2)
+
+    for j in range(len(coord_list)):
+        plt.scatter(coord_list[j][0], coord_list[j][1], c='black')
+    plt.show()
+
+
+#Calculate what cities ae connected given a radius
+def construct_fast_graph_connections(coord_list, radius):
+    place = ([])
+    distance_btw = ([])
+    tree = spatial.cKDTree(coords)
+
+    for i in range(len(coords)):
+        placce = tree.query_ball_point([coords[i][0], coords[i][1]], radius_search)
+        placce.remove(i)
+        for j in range(len(placce)):
+            place.append([i, placce[j]])
+            temp_dist = [math.sqrt(
+                math.pow(coords[i][0] - coords[placce[j]][0], 2) + math.pow(coords[i][1] - coords[placce[j]][1], 2))]
+            distance_btw.append(temp_dist)
+
+    return place, distance_btw
+
+#Constracts matrix with distances between connected cities
+def construct_graph(indices, distances, n):
+    x_coords = []
+    y_coords = []
+    range_right = []
+    for i_7 in range(len(indices)):
+        x_coords.append(indices[i_7][0])
+        y_coords.append(indices[i_7][1])
+        range_right.append(distances[i_7][0])
+
+    row = np.array(x_coords)
+    col = np.array(y_coords)
+    data = np.array(range_right)
+    end_matrix = csr_matrix((data, (row, col)), shape=(n, n)).toarray()
+
+    return end_matrix
+
+#Calculates the path/route to take from start til' end
+def compute_path(predecessor_matrix, start_node, end_node):
+    done = 1
+    run_node = end_node
+    final_path = ([])
+    final_path.append(end_node)
+    while done != 0:
+        #final_path.append(predecessor_matrix[start_node, run_node])
+        final_path.append(predecessor_matrix[run_node])
+        run_node = final_path[-1]
+        if final_path[-1] == start_node:
+            done = 0
+
+    return final_path
+
+
+#Calculates the total distance traveled
+def total_distance_traveled(coordinate_list, route_traveled):
+    total_distance_done=0
+    for i_99 in range(len(route_traveled)-1):
+        total_distance_done=total_distance_done+math.sqrt(math.pow(coordinate_list[route_traveled[i_99]][0]-coordinate_list[route_traveled[i_99+1]][0],2)+math.pow(coordinate_list[route_traveled[i_99]][1]-coordinate_list[route_traveled[i_99+1]][1],2))
+    return total_distance_done
+
+
+time_to_read = time.time()
+coordinate_list = read_coordinate_file(file_to_read)
+
+
+
+#Removes unwanted stuff from read file and creates a list with coordinates of all cities
+coords = ([])
+
+for i in range(len(coordinate_list)):
+
+    coordinate_list_split = coordinate_list[i].split(',')
+    coordinate_list_split[0] = coordinate_list_split[0].replace("{", "")
+    coordinate_list_split[1] = coordinate_list_split[1].replace("}", "")
+    coordinate_list_split[1] = coordinate_list_split[1].replace("\n", "")
+    coordinate_list_split[1] = coordinate_list_split[1].replace(" ", "")
+    coordinate_list_split[0] = math.log(math.tan(math.pi/4+math.pi*float(coordinate_list_split[0])/360))
+    coordinate_list_split[1] = float(coordinate_list_split[1])*math.pi/180
+
+    coordinate_list_split_temp = coordinate_list_split[0]
+    coordinate_list_split[0] = coordinate_list_split[1]
+    coordinate_list_split[1] = coordinate_list_split_temp
+    coords.append(coordinate_list_split)
+
+time_to_read = time.time() - time_to_read
+
+time_to_connections = time.time()
+placements, distance = construct_fast_graph_connections(coords, radius_search)
+time_to_connections = time.time() - time_to_connections
+
+time_to_matrix = time.time()
+matrix = construct_graph(placements, distance, len(coords))
+time_to_matrix = time.time() - time_to_matrix
+
+time_to_dijkstra = time.time()
+a, b = dijkstra(matrix, directed=False, indices=stad_1, return_predecessors=True, unweighted=False, limit=9999)
+time_to_dijkstra = time.time()-time_to_dijkstra
+
+time_to_path = time.time()
+route = compute_path(b, int(stad_1), int(stad_2))
+time_to_path = time.time()-time_to_path
+
+time_to_plot = time.time()
+plot_points(coords, placements, route)
+time_to_plot = time.time()-time_to_plot
+
+program_total_time = (time.time()-start_time) - time_to_plot
+
+absolute_distance = total_distance_traveled(coords,route)
+
+print("The time it takes for the whole program except plotting is: %4.3f \n"
+        "The time it takes to plot is: %4.3f \n"
+        "The time it takes to read coordinates from file is: %4.3f\n"
+        "The time it takes to construct connections is: %4.3f\n"
+        "The time it takes to construct the matrix is: %4.3f\n"
+        "The time it takes to find the shortest path, Dijkstas algorithm: %4.3f\n"
+        "The time it takes to find which path that should be taken: %4.3f\n"
+        "The total distance traveled: %4.3f\n"
+        % (program_total_time, time_to_plot, time_to_read, time_to_connections, time_to_matrix, time_to_dijkstra,
+         time_to_path, absolute_distance))
+
+print(route)
--- scratch.py
+++ scratch.py
@@ -1,9 +1,40 @@
 import numpy as np
 import math
+import time
 
+
+
+def user_input():
+    global radius
+    global start_node
+    global end_node
+    inp = input("What txt file do you want to read: Germany, Hungary or SampleCoordinates - Possible inputs: g, h and s")
+    if inp == "g" or inp == "G":
+        radius = 0.0025
+        start_node = 1573
+        end_node = 10584
+        return "GermanyCities.txt"
+    elif inp == "h" or inp == "H":
+        radius = 0.005
+        start_node = 311
+        end_node = 702
+        return "HungaryCities.txt"
+    elif inp == "s" or inp == "S":
+        radius = 0.08
+        start_node = 0
+        end_node = 5
+        return "SampleCoordinates.txt"
+
+
+comptime = []
+starttime_tot=time.time()
+
+filename=user_input()
+print(filename)
 
 # Uppgift 1, läser in, räknar raderna och använder det sedan som ett index för vilken rad koordinaterna motsvarar
 def read_coordinate_file(filename):
+    starttime=time.time()
     global coord_list
     global num_lines
     fileid = open(filename, 'r')
@@ -16,68 +47,179 @@ def read_coordinate_file(filename):
         y = math.log(math.tan(math.pi / 4 + float(temp[0][1:]) * math.pi / 360))
         coord_list[i, :] = [x, y]
         i += 1
+    fileid.close()
+    endtime = "Computational time for reading the txt file: {}".format(time.time() - starttime)
+    comptime.append(endtime)
 
 
-read_coordinate_file('SampleCoordinates.txt')
+read_coordinate_file(filename)
 
 # Uppgift 2 - Punkterna plottas ut
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib import colors as mcolors
 
-def plot_points(coord_list, connections):
+def plot_points(coord_list, connections, path):
+    starttime=time.time()
+    #uppgift 5
+    lines = []
+    the_way=[]
+    for x in range(0,connections.shape[0]):
+        line=[]
+        for k in range(0,2):
+            pointer = connections[x, k]
+            line.append((coord_list[int(pointer), 0], coord_list[int(pointer), 1]))
+        lines.append(line)
+
+    #Uppgift 8
+    for x in range(0,len(path)-1):
+        node=[]
+        for k in range(x,x+2):
+            pointer = path[k]
+            node.append((coord_list[pointer, 0], coord_list[pointer, 1]))
+
+        the_way.append(node)
+    reco_path = LineCollection(the_way, linewidths=(5), color='r')
+    line_segments = LineCollection(lines)
+
     fig1 = plt.figure()
-    fig1.gca()  # Only needed for the ipython %matplotlib inline to display something
-    plt.plot(coord_list, ".b")
-    LineCollection(connections)
+    ax = fig1.gca() # Only needed for the ipython %matplotlib inline to display something
+    plt.plot(coord_list[:, 0],coord_list[:,1], ".b")
+    ax.add_collection(line_segments)
+    ax.add_collection(reco_path)
 
     plt.show(block=False)
 
-plot_points(coord_list)
+    endtime = "Computational time for plotting points: {}".format(time.time() - starttime)
+    comptime.append(endtime)
+
 
 
 # Uppgift 3 -
 
 
 def construct_graph_connections(coord_list, radius):
-    global indices
-    global accDist
+    starttime=time.time()
     indices = np.array([0, 0])
     accDist = np.array([0])
-    for x in np.linspace(1, num_lines - 1, num_lines - 1):
-
-        for k in np.linspace(x + 1, num_lines, num_lines - x):
+    for x in range(1,num_lines):
+        for k in range(x+1, num_lines+1):
             dist = np.sqrt(((coord_list[int(k - 1), 0] - coord_list[int(x - 1), 0]) ** 2 + (
                     coord_list[int(k - 1), 1] - coord_list[int(x - 1), 1]) ** 2))
-
             if dist < radius:
                 indices = np.vstack((indices, [x, k]))
                 accDist = np.vstack((accDist,dist))
+            print(k)
+    indices = indices-1    #så att indexerna blir korrekta gentemot python syntaxen
+    indices = np.delete(indices, 0, axis=0)
+    for x in range(0, len(indices)):
+        indices = np.vstack((indices, [indices[x, 1], indices[x, 0]]))
+        accDist = np.vstack((accDist, accDist[x]))
+    accDist = np.delete(accDist, 0, axis=0)
+
+    endtime = "Computational time for constructing the graph connections: {}".format(time.time() - starttime)
+    comptime.append((endtime))
+    return indices, accDist
 
 
-radius = 0.08
-construct_graph_connections(coord_list, radius)
+#indices, accDist = construct_graph_connections(coord_list, radius)
 
-indices=np.delete(indices, 0, axis=0)
-accDist=np.delete(accDist, 0, axis=0)
+# här påkallas construct_graph_connections men den har kommenterats bort då den ger samma output som den snabbare
+
+
+# Uppgift 10
+import scipy.spatial as ss
+
+def construct_fast_graph_connections(coord_list, radius):
+    starttime = time.time()
+    temp = ss.cKDTree(coord_list)
+    indices_fast = np.array([0, 0])
+    dist_fast = np.array([0])
+    for x in range(len(temp.data)):
+        in_range = (temp.query_ball_point(coord_list[x], radius))
+        in_range.remove(x)
+        for k in range(len(in_range)):
+            temp_indices = [x, in_range[k]]
+            indices_fast = np.vstack((indices_fast, temp_indices))
+
+            dist = np.sqrt(((coord_list[x, 0] - coord_list[k, 0]) ** 2 + (
+                    coord_list[x, 1] - coord_list[k, 1]) ** 2))
+            dist_fast = np.vstack((dist_fast, dist))
+
+    indices_fast = np.delete(indices_fast, 0, axis=0)
+    dist_fast = np.delete(dist_fast, 0, axis=0)
+
+    endtime = "Computational time for constructing the fast graph connections: {}".format(time.time() - starttime)
+    comptime.append((endtime))
+    return indices_fast, dist_fast
+
+
+indices, accDist = construct_fast_graph_connections(coord_list, radius)
+
 
 
 # Uppgift 4
 N = len(coord_list)
 
-
 def construct_graph(indices, accdist, N):
+    starttime=time.time()
     from scipy.sparse import csr_matrix
     row = indices[:, 0]
     col = indices[:, 1]
     data = accdist[:, 0]
-    return csr_matrix((data, (row-1, col-1)), shape=( N, N))
+
+    endtime = "Computational time for constructing the graph: {}".format(time.time() - starttime)
+    comptime.append((endtime))
+
+    return csr_matrix((data, (row, col)), shape=(N, N)).toarray()
+
+# 0 = rad, 1 = kolumn, 2 = data
 
 
-construct_graph (indices, accDist, N)
-#7x7 matris som innehåller information om distanser mellan olika koordinater
+sparse = construct_graph(indices, accDist, N)
+
+
+#NxN matris som innehåller information om distanser mellan olika koordinater
 
 
 # Uppgift 5
 
 
+# Uppgift 6
+
+from scipy.sparse.csgraph import dijkstra
+
+starttimedijk = time.time()
+dijk = dijkstra(sparse, return_predecessors=True, directed=False, indices=start_node, unweighted=False)
+endtimedijk = endtime = "Computational time for dijkstra: {}".format(time.time() - starttimedijk)
+comptime.append((endtimedijk))
+
+
+# Uppgift 7
+def compute_path(predecessor_matrix, start_node, end_node):
+    starttime=time.time()
+    steps=[]
+    pos=end_node
+    while pos!=-9999:
+        steps.insert(0, pos)
+        pos=predecessor_matrix[pos]
+    endtime = "Computational time for computing the path: {}".format(time.time() - starttime)
+    comptime.append((endtime))
+    return steps
+
+
+shortest_info = [compute_path(dijk[1], start_node, end_node), dijk[0][end_node]]
+print("The shortest path is: {} with a total distance of: {}".format(shortest_info[0], shortest_info[1]))
+
+# Uppgift 8
+
+
+plot_points(coord_list, indices, shortest_info[0])
+
+# Uppgift 9
+
+endtime_tot = "Computational time for the whole program: {}".format(time.time() - starttime_tot)
+comptime.append(endtime_tot)
+print(comptime)
+
+plt.show()
