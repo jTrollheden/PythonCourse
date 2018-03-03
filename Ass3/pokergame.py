731f9cebdee09f2da64466657268a82cccfbf691
Joel Trollheden <joel@trollheden.com>
2018-03-03 15:22:10 +0100

    asd

--- Ass3/pokergame.py
+++ Ass3/pokergame.py
@@ -6,3 +6,14 @@ import logicTexas as log
 if __name__ == "__main__":
     [game_state, centercards, player1, player2] = log.execute()
     graphics.execute(game_state, centercards, player1, player2)
+
+# GUIdrawer - Programmet som ritar upp fönstret (basicGUIdrawer har ingen input från
+# någon annan fil AKA simple as it gets)
+
+# logicTexas - Min spellogik
+
+# cardlib - kort biblioteket
+
+# pokergame - Min "exe" fil typ
+
+# Vart jag är just nu: Koppla logiken med GUIn ytterligare samt skriva "Hur man vinner"
